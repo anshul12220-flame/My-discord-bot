556301797dd204594c1a89cbbcfbc34a6e0eeff2
anshul12220-flame <anshul12220@gmail.com>
2026-09-08 14:10:32 +0530

    Add basic bot code

--- Bot.py
+++ Bot.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run("YOUR_BOT_TOKEN")
